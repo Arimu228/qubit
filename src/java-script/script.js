f7debb159e38163b7252550540275f0db0678a8d
Ari <aaa100107aaa@gmail.com>
2022-05-15 14:20:50 +0600

    first commit

---
+++ src/java-script/script.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+    $(".carousel").owlCarousel({
+        items: 3,
+        loop: true,
+        autoplay:true,
+        autoplayHoverPause: true,
+        autoplaySpeed: 1000,
+
+    });
+});
